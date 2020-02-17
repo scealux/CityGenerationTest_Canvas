2b0aec9c7f83af9124686c5a7f08abf17031f49b
Alex Fiel <alfi1868@colorado.edu>
2020-02-16 21:58:45 -0700

    Closer to testable...
    Added:
    - Cap Coords
    Decided capitals are invisible influences
    - Made progress on placement function
    - Added function to find capital values in arrays
    
    TODO:
    Piece together placement function
    
    Draw information to canvas
    
    create export to json  and import functions
    
    add weighted centroid modifier
    
    test, test, test

--- src/index.js
+++ src/index.js
@@ -1,13 +1,10 @@
 //Global Variables
 var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
-var buildings = [];
+
+var capA = []; //active capitals
+var capD = []; //dormant capitals
 var sites = [];
-var hostL = [],
-  tagL = [],
-  toolL = [],
-  topicL = [],
-  categoryL = [];
 
 //Setup Buttons
 var fromJSONButton = document.getElementById("fromJSON");
@@ -28,57 +25,60 @@ logButton.addEventListener("click", logData);
 //Get input from text fields on demand
 var sub = {
   method: function() {
-    return document.getElementById("title").value;
+    return document.getElementById("title").value.toLowerCase();
   },
   title: function() {
-    return document.getElementById("title").value;
+    return document.getElementById("title").value.toLowerCase();
   },
   host: function() {
-    return document.getElementById("host").value;
+    return document.getElementById("host").value.toLowerCase();
   },
   tags: function() {
-    return document.getElementById("tags").value.split(",");
+    return document
+      .getElementById("tags")
+      .value.split(",")
+      .forEach(function(val) {
+        val = val.toLowerCase();
+      });
   },
   tool: function() {
-    return document.getElementById("tool").value;
+    return document.getElementById("tool").value.toLowerCase();
   },
   topic: function() {
-    return document.getElementById("topic").value;
+    return document.getElementById("topic").value.toLowerCase();
   },
   category: function() {
-    return document.getElementById("category").value;
+    return document.getElementById("category").value.toLowerCase();
   }
 };
 
 //Weights for categories
-var titleW = 0.5;
-var hostW = 0.5;
-var tagW = 0.5;
-var toolW = 0.5;
-var topicW = 0.5;
-var categoryW = 0.5;
+var weights = [
+  0.1, //host
+  0.2, //tags
+  0.3, //tool
+  0.4, //topic
+  0.5 //category
+];
 
 //Objects
-function building(id, isCapital, x, y, numSites) {
-  this.id = id;
-  this.isCapital = isCapital;
+function capital(id, x, y) {
+  this.id = id; //Keyword
   this.x = x;
   this.y = y;
-  this.numSites = numSites;
 }
 
-function site(building, title, host, tags, tool, topic, category, isCapital) {
-  this.building = building;
+function site(capital, title, host, tags, tool, topic, category) {
+  this.capital = capital;
   this.title = title;
   this.host = host;
   this.tags = tags;
   this.tool = tool;
   this.topic = topic;
   this.category = category;
-  this.isFixed = isCapital;
   this.floor = 0;
   this.xy = function() {
-    return [building.x, building.y];
+    return [capital.x, capital.y];
   };
 }
 
@@ -89,12 +89,9 @@ function init() {
   canvas.height = window.innerHeight - 200;
 
   //Clear variables
-  buildings = [];
+  capA = [];
+  capD = [];
   sites = [];
-
-  //Populate city
-  //For the 9ish chunks surrounding the user...
-  //fromJSON();
 
   //Begin update loop
   window.requestAnimationFrame(draw);
@@ -118,91 +115,152 @@ function addNewSite() {
   if (sub.title() !== "") {
     console.log("Adding new node...");
     var nSite = new site(
-      0, //building
+      0, //capital
       sub.title(),
       sub.host(),
       sub.tags(),
       sub.tool(),
       sub.topic(),
-      sub.category(),
-      false //isCapital
+      sub.category()
     );
 
     var influences = getInfluences();
-    console.log("With influences: ");
     console.log(influences);
-    var numBuildings = buildings.length;
 
-    if (numBuildings === 0) {
-      //If it's the first building...
-      //Add it in the center and make it the capital of its highest weighted influence
-      //Find heighest weighted influence
-      var nBuild = new building(influences[1], true, 0, 0, 1); //id, isCapital, x, y, numSites
-      [];
-    } else {
-      //Read textbox values
-      // var info = {};
-      //Read checkbox values into "info"
-      //if (x.checked){
-      //info.push(x[i].id)
-      //}
-      //New NODE with VALUES
-      //Find capitals for checked inputs
-      //var R = [];
+    //For unique capitals, add them
+
+    //Then find the capitals coordinates
+    //Feed them into centroid
+
+    //Check for buildings around centroid;
+    //if none, create one.
+    //Add site with reference to building
+
+    if (sites.length === 0) {
+      //If it's the first site...
+      for (var i = 0; i < influences.length; i++) {
+        //Make capitals for each of the unique influences
+        var newCoords = capCoords();
+        var nCap = new capital(
+          influences[i].value, //IDs as influence
+          newCoords.x, //
+          newCoords.y
+        );
+        capA.push(nCap);
+      }
     }
+    //Find capitals
+    for (var i = 0; i < capA.length; i++) {}
   } else {
     console.log("Didn't add, no title.");
   }
 }
 
+function capCoords() {
+  var newCoords = [];
+  var inDonut = false;
+  var outer = 60;
+  var inner = 30;
+  var rnd = randInt(0, capA.length);
+  for (var i = 0; i < 50; i++) {
+    while (!inDonut) {
+      newCoords = [
+        //Create a new coordinate in a range from the center one
+        randInt(capA[rnd].x - outer, capA[rnd].x + outer),
+        randInt(capA[rnd].y - outer, capA[rnd].y + outer)
+      ];
+      var dist = distance(capA[rnd].x, capA[rnd].y, newCoords[0], newCoords[1]);
+      if (dist < outer && dist > inner) {
+        inDonut = true;
+      }
+    }
+    //For each point in the donut, check the distance to each active capital...
+    var passActive = true;
+    for (var j = 0; j < capA.length; j++) {
+      dist = distance(capA[j].x, capA[j].y, newCoords[0], newCoords[1]);
+
+      if (dist < inner) {
+        //if it's too close to any point, stop.
+        passActive = false;
+        break;
+      }
+    }
+
+    if (passActive) {
+      var passDormant = true;
+      //Passed all the active points
+      for (j = 0; j < capD.length; j++) {
+        //For each capital
+        dist = distance(capD[j].x, capD[j].y, newCoords[0], newCoords[1]);
+
+        if (dist < inner) {
+          //if it's too close to any point, stop.
+          passDormant = false;
+          break;
+        }
+      }
+    }
+
+    if (passDormant && passActive) {
+      return newCoords; //If it passed both tests, it's a valid point
+    }
+    // If it didn't pass, try again with a new point
+  }
+  // Makes it here if it tried 50 times and couldn't find a valid point.
+  capD.push(capA[rnd]); // Move that capital to dormant list
+  capA.splice(rnd, 1); // Deletes that capital from the active list
+}
+
+function distance(x1, y1, x2, y2) {
+  return Math.hypot(x1 - x2, y1 - y2);
+}
+
+function randInt(min, max) {
+  return Math.floor(Math.random * (max - min) + min);
+}
+
 function getInfluences() {
-  //Collect influences as well as higest weighted one.
+  // Collect influences
   var checks = document.getElementsByClassName("nodeCheck");
-  var highest = 0;
   var infs = [];
   for (var i = 0; i < checks.length; i++) {
     if (checks[i].checked === true) {
-      //If an influence is checked
-      var cat = checks[i].id.split("-")[0]; //Derive the category
-      var z = new influ(cat, sub[cat]()); //Create new influence
-      infs.push(z); //Add it to the influences
-
-      //START HERE
-      console.log([String(cat + "W")]);
-      // if ([String(cat + "W")] > highest){
-
-      // }
+      // If an influence is checked
+      var cat = checks[i].id.split("-")[0]; // Derive the category
+      var z = new influ(cat, sub[cat](), weights[i]); // Create new influence
+      infs.push(z); // Add it to the influences
     }
   }
   return infs;
 }
 
-function influ(category, value) {
+function influ(category, value, weight) {
   this.category = category;
   this.value = value;
+  this.weight = weight;
 }
 
 function centroidOfPoints(points) {
-  //NOT TESTED
-  //add all the poitns together and
-  var xs = 0; //totals x values
-  var ys = 0; //totals y values
-  var tot = points.length / 2; //number of coordinates
+  // NOT TESTED
+  // add all the poitns together and
+  var xs = 0; // totals x values
+  var ys = 0; // totals y values
+  var tot = points.length / 2; // number of coordinates
   for (var i = 0; i < tot; i += 2) {
-    //add each to the total
+    // add each to the total
     xs += points[i];
     ys += points[i + 1];
   }
-  return [xs / tot, ys / tot]; //return average
+  return [xs / tot, ys / tot]; // return average
 }
 
-function findInfoinData(x, y, info) {
-  for (var i = 0; i < y.length; i++) {
-    if (y[i][info] == x) {
-      return y[i];
-    }
+function findCap(value) {
+  // Find a capital where its value matches
+  var cap = capD.find(element => element.id == value); // Check dormant
+  if (cap == undefined) {
+    cap = capA.find(element => element.id == value); // Check active
   }
-  return false;
+  return cap; // Return the found value (could be undefined)
 }
 
 function calculateCentroid(x1, y1, x2, y2, x3, y3) {
@@ -218,7 +276,7 @@ function clearFields() {
   }
   var checks = document.getElementsByClassName("nodeCheck");
   //console.log(checks);
-  for (var i = 0; i < checks.length; i++) {
+  for (i = 0; i < checks.length; i++) {
     checks[i].checked = false;
   }
   console.log("Fields Cleared");
@@ -233,7 +291,7 @@ function fromJSON() {
   console.log("From JSON");
   //Place Anchors at Points from storage
   //Read JSON for "chunk"
-  //For each building in JSON, convert it to object and push to buildings[]
+  //For each capital in JSON, convert it to object and push to capitals[]
   //For each site in JSON, convert to object and push to sites[];
 }
 
